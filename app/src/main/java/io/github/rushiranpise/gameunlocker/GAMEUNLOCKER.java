20003e4e75c684228840d2bbbea6dfbb382bb4c6
irfaantx <67856365+irfaantx@users.noreply.github.com>
2023-11-21 12:01:34 +0700

    Update GAMEUNLOCKER.java

--- app/src/main/java/io/github/rushiranpise/gameunlocker/GAMEUNLOCKER.java
+++ app/src/main/java/io/github/rushiranpise/gameunlocker/GAMEUNLOCKER.java
@@ -40,79 +40,79 @@ public class GAMEUNLOCKER implements IXposedHookLoadPackage {
         
         if (Arrays.asList(PACKAGE_ROG1).contains(packageName)) {
             ROG1();
-            XposedBridge.log("Spoofed" + packageName + " as Asus ROG 1");
+            XposedBridge.log("Spoofed " + packageName + " as Asus ROG 1");
 
         }
 
         if (Arrays.asList(PACKAGE_ROG3).contains(packageName)) {
             ROG3();
-            XposedBridge.log("Spoofed" + packageName + " as Asus ROG 3");
+            XposedBridge.log("Spoofed " + packageName + " as Asus ROG 3");
 
         }
 
         if (Arrays.asList(PACKAGE_OP8P).contains(packageName)) {
             OP8P();
-            XposedBridge.log("Spoofed" + packageName + " as OnePlus 8 Pro");
+            XposedBridge.log("Spoofed " + packageName + " as OnePlus 8 Pro");
 
         }
 
         if (Arrays.asList(PACKAGE_OP9R).contains(packageName)) {
             OP9RP();
-            XposedBridge.log("Spoofed" + packageName + " as OnePlus 9R");
+            XposedBridge.log("Spoofed " + packageName + " as OnePlus 9R");
 
         }
 
         if (Arrays.asList(PACKAGE_M11TP).contains(packageName)) {
             M11TP();
-            XposedBridge.log("Spoofed" + packageName + " as Mi 11T Pro");
+            XposedBridge.log("Spoofed " + packageName + " as Mi 11T Pro");
 
         }
 
         if (Arrays.asList(PACKAGE_ROG6).contains(packageName)) {
             ROG6();
-            XposedBridge.log("Spoofed" + packageName + " as Asus ROG 6");
+            XposedBridge.log("Spoofed " + packageName + " as Asus ROG 6");
  
         }
 
         if (Arrays.asList(PACKAGE_OP9P).contains(packageName)) {
             OP9P();
-            XposedBridge.log("Spoofed" + packageName + " as OnePlus 9 Pro");
+            XposedBridge.log("Spoofed " + packageName + " as OnePlus 9 Pro");
 
         }
 
         // if (Arrays.asList(PACKAGE_M13TP).contains(packageName)) {
         //     M13TP();
-        //     XposedBridge.log("Spoofed" + packageName + " as  Mi 13T Pro");
+        //     XposedBridge.log("Spoofed " + packageName + " as  Mi 13T Pro");
 
         // }
 
         if (Arrays.asList(PACKAGE_F5).contains(packageName)) {
             POCOF5();
-            XposedBridge.log("Spoofed" + packageName + " as Poco F5");
+            XposedBridge.log("Spoofed " + packageName + " as Poco F5");
 
         }
 
         if (Arrays.asList(PACKAGE_K30U).contains(packageName)) {
             K30U();
-            XposedBridge.log("Spoofed" + packageName + " as Redmi K30 Ultra");
+            XposedBridge.log("Spoofed " + packageName + " as Redmi K30 Ultra");
 
         }
 
         if (Arrays.asList(PACKAGE_M11U).contains(packageName)) {
             M11U();
-            XposedBridge.log("Spoofed" + packageName + " as Mi 11 Ultra");
+            XposedBridge.log("Spoofed " + packageName + " as Mi 11 Ultra");
 
         }
 
         if (Arrays.asList(PACKAGE_Y700).contains(packageName)) {
             Y700();
-            XposedBridge.log("Spoofed" + packageName + " as Lenovo Legion Y700");
+            XposedBridge.log("Spoofed " + packageName + " as Lenovo Legion Y700");
 
         }
 
         if (Arrays.asList(PACKAGE_V2254A).contains(packageName)) {
             V2254A();
-            XposedBridge.log("Spoofed" + packageName + " as iQOO 11 Pro");
+            XposedBridge.log("Spoofed " + packageName + " as iQOO 11 Pro");
 
         }
         
@@ -187,7 +187,7 @@ public class GAMEUNLOCKER implements IXposedHookLoadPackage {
     }
 
     private static void Y700() {
-        setBuildField("MANUFACTURER", "lenovo");
+        setBuildField("MANUFACTURER", "Lenovo");
         setBuildField("MODEL", "Lenovo TB-9707F");
     }
 
@@ -206,10 +206,10 @@ public class GAMEUNLOCKER implements IXposedHookLoadPackage {
             // Lock
             field.setAccessible(false);
             
-            Log.d(TAG, "Spoofed" + key + " with value: " + value);
+            Log.d(TAG, "Spoofed " + key + " with value: " + value);
             
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            Log.e(TAG, "Failed to spoof App." + key, e);
+            Log.e(TAG, "Failed to spoof the application. " + key, e);
         }
     }
 
